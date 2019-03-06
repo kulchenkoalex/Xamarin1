5f22779e208877df7b4e7eea4ff8350e3d24e6e3
alexey.kulchenko@swanteams.com <alexey.kulchenko@swanteams.com>
2019-03-06 12:16:58 +0200

    Added TabbedPage and MapPage

--- HelloApp/HelloApp/LoginPage.xaml.cs
+++ HelloApp/HelloApp/LoginPage.xaml.cs
@@ -24,7 +24,7 @@ namespace HelloApp
         {
             if (emailEntry.Text == emailAdmin && passwordEntry.Text == passwordAdmin)
             {
-                Navigation.PushAsync(new MainPage());
+                Navigation.PushAsync(new MenuPage());
             }
             else
             {
---
+++ HelloApp/HelloApp/MapPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Plugin.Geolocator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+        }
+
+        //protected async override void OnAppearing()
+        //{
+        //    base.OnAppearing();
+
+        //    var locator = CrossGeolocator.Current;
+        //    locator.PositionChanged += Locator_PositionChanged;
+        //    await locator.StartListeningAsync(0, 100);
+
+        //    var position = await locator.GetPositionAsync();
+
+        //    var center = new Xamarin.Forms.Maps.Position(position.Latitude, position.Longitude);
+        //    var span = new Xamarin.Forms.Maps.MapSpan(center, 2, 2);
+        //    locationsMap.MoveToRegion(span);
+        //}
+
+        //private void Locator_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        //{
+        //    var center = new Xamarin.Forms.Maps.Position(e.Position.Latitude, e.Position.Longitude);
+        //    var span = new Xamarin.Forms.Maps.MapSpan(center, 2, 2);
+        //    locationsMap.MoveToRegion(span);
+        //}
+    }
+}
